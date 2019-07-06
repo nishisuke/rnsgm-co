d1667e42f1efea21769e7a96a8dd2cf94a6b5b4c
nishisuke <ryoskns+git@gmail.com>
2019-07-06 09:39:35 +0900

    f

--- docs/scroll.js
+++ docs/scroll.js
@@ -1,5 +1,5 @@
 console.log('fuga')
-document.body.addEventListener('touchmove', function(e) {
+document.addEventListener('touchmove', function(e) {
   console.log('hoge')
   e.preventDefault()
 }, { passive: false })
